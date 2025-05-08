642b5513d896538b34f4a3dcc249ae787cedab43
Jeong <jeongm0220@gmail.com>
2025-05-08 19:45:15 +0900

    Initial commit

---
+++ src/main/java/com/comma/geuneulgil/GeuneulgilBackendApplication.java
@@ -0,0 +1,13 @@
+package com.comma.geuneulgil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeuneulgilBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeuneulgilBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/comma/geuneulgil/GeuneulgilBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comma.geuneulgil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeuneulgilBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
